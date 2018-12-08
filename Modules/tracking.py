e8e3f5082ebead9478cc37f1a35bfca32942ba0a
erpaul <43856942+erpaul@users.noreply.github.com>
2018-12-07 23:52:49 -0600

    comments

--- Modules/tracking.py
+++ Modules/tracking.py
@@ -11,24 +11,24 @@ scidata = hdulist[0].data
 #2 pixels wider than array in both dimensions for boundry conditions of get_nbr function
 pixels_checked=np.zeros((len(scidata)+2, len(scidata[0])+2))
 for x in range(len(scidata[0])+2):
-    pixels_checked[0][x]=1
-    pixels_checked[len(pixels_checked)-1][x]=1
+    pixels_checked[0,x]=1
+    pixels_checked[len(pixels_checked)-1, x]=1
 for y in range(len(scidata)+2):
-    pixels_checked[y][0]=1
-    pixels_checked[y][len(pixels_checked[0])-1]=1
+    pixels_checked[y,0]=1
+    pixels_checked[y, len(pixels_checked[0])-1]=1
 for x in range(len(scidata[0])):
     for y in range(len(scidata)):
-        if scidata[y,x] == 0:
-            pixels_checked[y+1][x+1]=1
+        if scidata[y][x] == 0:
+            pixels_checked[y+1, x+1]=1
 
 #append to array and change pixels_checked value            
 def ap (tpl, x, y):
     tpl.append([y,x])
-    pixels_checked[y+1][x+1]=1
+    pixels_checked[y+1, x+1]=1
 
 #check if pixel has already been checked
 def if_checked(x,y):
-    if pixels_checked[y+1][x+1]==1:
+    if pixels_checked[y+1, x+1]==1:
         return("yes")
     else:
         return("no")
@@ -48,10 +48,9 @@ def get_track(trackmap, x,y):
 
 def get_nbr(trackmap, x,y):
     if y<len(scidata) and x<len(scidata[0]):
-        if scidata[y,x]==1 and  if_checked(x,y)=="no":   #would use pixel_checked array to check validity, but this always gives stack error
+        if scidata[y][x]==1 and  if_checked(x,y)=="no":   #would use pixel_checked array to check validity, but this always gives stack error
             ap(trackmap, x,y)
             get_track(trackmap, x,y)
         return
         
-
 
